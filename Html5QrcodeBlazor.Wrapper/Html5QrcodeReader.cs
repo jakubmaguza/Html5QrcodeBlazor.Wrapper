03d83da0712db97166edd21f5adc8cd68a866f00
Jakub Maguza <jakub.maguza@gmail.com>
2023-05-03 22:06:42 +0200

    Add project files.

---
+++ Html5QrcodeBlazor.Wrapper/Html5QrcodeReader.cs
@@ -0,0 +1,62 @@
+﻿using Html5QrcodeBlazor.Wrapper.Util;
+using Microsoft.JSInterop;
+
+namespace Html5QrcodeBlazor.Wrapper
+{
+    public class Html5QrcodeReader : JSModule
+    {
+        public bool IsCameraInitialized { get; set; } = false;
+        private DotNetObjectReference<Html5QrcodeReader>? dotNetHelper;
+        /// <summary>
+        /// Event raised on successfull barcode scan
+        /// </summary>
+        public event Action<string>? OnBarcodeScan;
+        public Html5QrcodeReader(IJSRuntime js)
+            : base(js, "./_content/Html5QrcodeBlazor.Wrapper/barcodeScanner.js")
+        {
+
+        }
+        public async Task Start(string divId = "reader", string facingMode = "environment", int fps = 10)
+        {
+            if (IsCameraInitialized)
+                return;
+            dotNetHelper ??= DotNetObjectReference.Create(this);
+            await InvokeVoidAsync("setDotNetHelper", dotNetHelper);
+            await InvokeVoidAsync("initializeHtml5Qrcode", divId, facingMode, fps);
+            IsCameraInitialized = true;
+        }
+        /// <summary>
+        /// Easy Mode - With end to end scanner user interface
+        /// </summary>
+        public async Task Render(string divId = "reader", int fps = 10)
+        {
+            if (IsCameraInitialized)
+                return;
+            dotNetHelper ??= DotNetObjectReference.Create(this);
+            await InvokeVoidAsync("setDotNetHelper", dotNetHelper);
+            await InvokeVoidAsync("renderHtml5Qrcode", divId, fps);
+            IsCameraInitialized = true;
+        }
+        public async Task Stop()
+        {
+            if (!IsCameraInitialized)
+                return;
+
+            try
+            {
+                await InvokeVoidAsync("stopCamera");
+            }
+            catch (Exception)
+            {
+                //skip
+            }
+            IsCameraInitialized = false;
+        }
+        [JSInvokable("HandleCameraBarcodeScan")]
+        public void HandleCameraBarcodeScan(string barcode)
+        {
+            OnBarcodeScan?.Invoke(barcode);
+        }
+
+    }
+}
---
+++ Html5QrcodeBlazor.Wrapper/wwwroot/barcodeScanner.js
@@ -0,0 +1,21 @@
+let dotNetHelper;
+let html5QrCode;
+let html5QrCodeScanner;
+
+export async function initializeHtml5Qrcode(divName, facingMode, fps) {
+    html5QrCode = new Html5Qrcode(divName);
+    html5QrCode.start({ facingMode: facingMode }, {fps: fps}, handleCameraBarcodeScan);
+}
+export async function renderHtml5Qrcode(divName, fps, verbose) {
+    html5QrCodeScanner = new Html5QrcodeScanner(divName, { fps: fps }, verbose);
+    html5QrCodeScanner.render(handleCameraBarcodeScan);
+}
+export async function stopCamera() {
+    await html5QrCode?.stop();
+}
+export async function setDotNetHelper(value) {
+    dotNetHelper = value;
+}
+async function handleCameraBarcodeScan(barcode, data) {
+    await dotNetHelper.invokeMethodAsync('HandleCameraBarcodeScan', barcode);
+}
